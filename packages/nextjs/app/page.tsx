f7520666fec13919e8512945ec1eee109df0c0a0
idspn <114414329+idspn@users.noreply.github.com>
2024-12-22 06:29:27 +0500

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -80,7 +80,6 @@ const Home: NextPage = () => {
         <div className="card bg-base-100 w-96 shadow-xl">
           <div className="card-body">
             <h2 className="card-title">Игра</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
               <button className="btn btn-primary" onClick = {() => play()}>
                 Играть</button>
